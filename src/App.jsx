8d599666a56c89f4768d9c28fefa85010fd73181
magic-doggo <109861946+magic-doggo@users.noreply.github.com>
2025-03-08 14:09:49 +0000

    Shuffle pokemon array order to change display order after each click

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,26 @@ function App() {
   const [currentScore, setCurrentScore] = useState(0);
   const [clickedPokemon, setClickedPokemon] = useState([]);
   const [bestScore, setBestScore] = useState(0);
-  let nrOfDesiredPokemon = 2;
+  let nrOfDesiredPokemon = 5;
+  //randomize list of x (nrofdesiredpokemon) number using Fisher–Yates shuffle/durstenfeld version
+  //generate random pokeList and map images based on it?
+
+  //fisher-yates shuffle algo: https://javascript.info/array-methods
+  function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  }
 
   useEffect(() => {
     async function getPokemon(nrOfPokemon) {
       let tempPokeList = [];
-      for (let i = 1; i < (nrOfPokemon +1) ; i++) {
+      for (let i = 1; i < (nrOfPokemon + 1); i++) {
         const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`);
         const pokemonData = await response.json();
-        let pokemonObject = { name: pokemonData.forms[0].name, icon: pokemonData.sprites.front_default, pokeIndex: i}
+        let pokemonObject = { name: pokemonData.forms[0].name, icon: pokemonData.sprites.front_default, pokeIndex: i }
         tempPokeList.push(pokemonObject);
       };
       console.log("called the api loop")
@@ -24,23 +35,29 @@ function App() {
     getPokemon(nrOfDesiredPokemon);
   }, []);
 
+
   function submitClickedPokemon(pokeNr) {
+    
+    const tempArray = [...pokeList];
+    shuffle(tempArray);
+    setPokeList(tempArray);
+
     if (clickedPokemon.includes(pokeNr)) {
       // youLost();
       gameFinished();
       console.log('you Lost')
     } else {
       setClickedPokemon([...clickedPokemon, pokeNr]);
-      let tempScore = currentScore +1;
-      setCurrentScore(currentScore +1);
+      let tempScore = currentScore + 1;
+      setCurrentScore(currentScore + 1);
       shuffleCards();
       if (tempScore > bestScore) setBestScore(tempScore);
-      if (clickedPokemon.length === (nrOfDesiredPokemon -1)) {
+      if (clickedPokemon.length === (nrOfDesiredPokemon - 1)) {
         console.log('you won')
         // youWon();
         gameFinished();
       }
-      
+
     }
   }
 
@@ -72,7 +89,7 @@ function App() {
 
       {pokeList.map((pokemon) => (
         <Card key={pokemon.pokeIndex} imageURL={pokemon.icon} name={pokemon.name}
-        executeOnClick={() => submitClickedPokemon(pokemon.pokeIndex)} ></Card>
+          executeOnClick={() => submitClickedPokemon(pokemon.pokeIndex)} ></Card>
       ))}
     </div>
   )
